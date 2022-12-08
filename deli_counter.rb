ae575a3d8be1e7db864436176c92f7b4534ef3e5
Steven McVey <smcvey1210@gmail.com>
2022-12-07 18:05:16 -0800

    commit tree 9838364826c277bf6a11ab4db9d9850cee5ae4e6

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,34 @@
 # Write your code here.
+require 'pry'
+
+katz_deli = []
+
+def line(customers)
+  if customers.length == 0
+    puts "The line is currently empty."
+  else
+    x = 0
+    list = customers.collect do |person|
+      x += 1
+      "#{x}. #{person}"
+    end
+    puts "The line is currently: #{list.join(" ")}"
+  end
+end
+
+def take_a_number(array, name)
+  array << name
+  x = array.length
+  puts "Welcome, #{name}. You are number #{x} in line."
+end
+
+take_a_number(katz_deli, "Ada")
+
+def now_serving(array)
+  if array.length != 0
+    puts "Currently serving #{array[0]}."
+    array.shift
+  else
+    puts "There is nobody waiting to be served!"
+  end
+end
